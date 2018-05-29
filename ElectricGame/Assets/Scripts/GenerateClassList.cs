1196258f26ebbdda01ae636ef056ed628b359899
Grumpy-Kat <litvin.ariela@lmghs.org>
2018-05-28 20:19:28 -0400

    Names generated from user-editable list

--- ElectricGame/Assets/Scripts/GenerateClassList.cs
+++ ElectricGame/Assets/Scripts/GenerateClassList.cs
@@ -1,92 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class GenerateClassList : MonoBehaviour
 {
     [SerializeField]
-    int NumCitizens;
+    int numCitizens = 20;
 
     [SerializeField]
-    int AgeMin = 20;
+    int ageMin = 18;
 
     [SerializeField]
-    int AgeMax = 80;
+    int ageMax = 80;
     
 
-    public List<Citizen> Population = new List<Citizen>();
+    public List<Citizen> population = new List<Citizen>();
 
-    List<string> NamesList = new List<string>();
+    List<string> namesList;
 
     int currentCitizen = 0;
     // Use this for initialization
-    void Start ()
-    {
-        StartList();
-
-        for (int i = 0; i < NumCitizens; i++)
-        {
-            Population.Add(new Citizen(NamesList[Random.Range(0, NamesList.Count - 1)],Random.Range(AgeMin,AgeMax)));
-        }
-
+    void Start () {
+		GenerateNames();
+		for (int i = 0; i < numCitizens; i++) {
+            population.Add(new Citizen(namesList[Random.Range(0, namesList.Count-1)],Random.Range(ageMin, ageMax)));
+       }
 	}
 
-    void Update()
-    {
-    }
-
-    void StartList()
-    {
-        NamesList.Add("Ryan");
-        NamesList.Add("Marcella");
-        NamesList.Add("Shalon");
-        NamesList.Add("Abram");
-        NamesList.Add("Almeta");
-        NamesList.Add("Luetta");
-        NamesList.Add("Bethann");
-        NamesList.Add("Judson");
-        NamesList.Add("Alida");
-        NamesList.Add("Shayla");
-        NamesList.Add("Trista");
-        NamesList.Add("Albert");
-        NamesList.Add("Ramonita");
-        NamesList.Add("Ngan");
-        NamesList.Add("Rebeca");
-        NamesList.Add("Cedrick");
-        NamesList.Add("Shamika");
-        NamesList.Add("Particia");
-        NamesList.Add("Rana");
-        NamesList.Add("Ligia");
-        NamesList.Add("Markus");
-        NamesList.Add("Genia");
-        NamesList.Add("Kara");
-        NamesList.Add("Zonia");
-        NamesList.Add("Eusebio");
-        NamesList.Add("Leeann");
-        NamesList.Add("Kerrie");
-        NamesList.Add("Sherryl");
-        NamesList.Add("Shan");
-        NamesList.Add("Elda");
-        NamesList.Add("Ula");
-        NamesList.Add("Hugo");
-        NamesList.Add("Thora");
-        NamesList.Add("Demetric"); 
-        NamesList.Add("Stefania");
-        NamesList.Add("Sha");
-        NamesList.Add("Nelida");
-        NamesList.Add("Amos");
-        NamesList.Add("Lashell");
-        NamesList.Add("Billye");
-        NamesList.Add("Bok");
-        NamesList.Add("Kurtis");
-        NamesList.Add("Yuki");
-        NamesList.Add("Margert");
-        NamesList.Add("Sung");
-        NamesList.Add("Anamaria"); 
-        NamesList.Add("Hermelinda");
-        NamesList.Add("Claris");
-        NamesList.Add("Ashly");
-        NamesList.Add("Delores");
+	void GenerateNames(){
+		string names = File.ReadAllText(Application.streamingAssetsPath + "/names.txt");
+		namesList = new List<string>(names.Split('\n'));
     }
 
     public void NextCit()
