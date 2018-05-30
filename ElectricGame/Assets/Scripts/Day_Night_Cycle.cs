74ab7d6e6bcab64f5beab9d719abaa7797ee212d
Purn <purnjay19@gmail.com>
2018-05-30 13:21:16 +0530

    Update : Day_Night_Cycle
    Made Day_Night_Cycle Script and also a scene to demonstrate it.

---
+++ ElectricGame/Assets/Scripts/Day_Night_Cycle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Day_Night_Cycle : MonoBehaviour {
+
+   delegate void myDelegate();
+    myDelegate my;
+
+    public float cycleSpeed = 5f;
+
+
+	// Use this for initialization
+	void Start () {
+
+        my += CycleInfo;
+      
+	}
+	
+    void Update() {
+        my();
+
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.RotateAround(Vector3.zero, Vector3.right, cycleSpeed * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+    }
+
+    void CycleInfo()
+    {
+        
+        if (GameObject.FindGameObjectWithTag("Sun").transform.position.y < 0f)
+        {
+            Debug.Log("it's night!");
+        }
+      
+    }
+}
