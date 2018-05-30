201cfbcc3025003529a1cb2f0399c2e794ec0d91
Barney25052 <Barney25052@users.noreply.github.com>
2018-05-30 19:31:48 +0100

    .
    .

---
+++ ElectricGame/Assets/Scripts/PowerGrid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerGrid : MonoBehaviour {
+    
+    public List<List<float>> powerGrid = new List<List<float>>();
+    
+    public void MakePowerGrid (int buildingLength, int buildingHeight) {
+        for (int x = 0; x < buildingLength; x++)
+        {
+            List<float> powerValues = new List<float>();
+            for (int y = 0; y < buildingHeight; y++)
+            {
+                powerValues.Add(100);
+            }
+            powerGrid.Add(powerValues);
+        }
+    }
+}
